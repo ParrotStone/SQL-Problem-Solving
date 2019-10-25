c5d5669adb535e714f60bc2052c08f0da2c993cb
Abdulrahmn Ali Hussain <abdulrahmanalihussainM@gmail.com>
2019-10-25 22:03:44 +0200

    Added another solutions to old SQL challenges

--- Not-boring-movies/query.sql
+++ Not-boring-movies/query.sql
@@ -5,3 +5,9 @@ WHERE cinema.id % 2 <> 0
 	 AND cinema.description <> 'boring'
 		ORDER BY cinema.rating DESC;
 
+
+-- Another solution
+# SELECT *
+# FROM cinema
+# WHERE mod(id, 2) <> 0 AND description != 'boring'
+# ORDER BY rating DESC;
